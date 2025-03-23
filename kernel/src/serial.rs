ea55641ebb744b846d2568eb7f4b5b1d7ea1c7cf
CordlessCoder <roman@vm.net.ua>
2025-03-23 15:30:13 +0000

    Remove interrupt disabling logic for write/print macros as that's now handled by the spinlock

--- kernel/src/serial.rs
+++ kernel/src/serial.rs
@@ -17,27 +17,17 @@ pub fn _print(args: ::core::fmt::Arguments) {
 /// Prints to the host through the serial interface.
 #[macro_export]
 macro_rules! serial_print {
-    (interrupts_disabled $($arg:tt)*) => {
-        $crate::serial::_print(format_args!($($arg)*));
-    };
     ($($arg:tt)*) => {
-        ::x86_64::instructions::interrupts::without_interrupts(|| {
-            $crate::serial::_print(format_args!($($arg)*));
-        })
+        $crate::serial::_print(format_args!($($arg)*));
     };
 }
 
 /// Prints to the host through the serial interface, appending a newline.
 #[macro_export]
 macro_rules! serial_println {
-    (interrupts_disabled) => (interrupts_disabled $crate::serial_print!("\n"));
-    (interrupts_disabled $fmt:expr) => ($crate::serial_print!(interrupts_disabled concat!($fmt, "\n")));
-    (interrupts_disabled $fmt:expr, $($arg:tt)*) => {
-        $crate::serial_print!(interrupts_disabled concat!($fmt, "\n"), $($arg)*);
+    () => ($crate::serial_print!("\n"));
+    ($fmt:expr) => ($crate::serial_print!(concat!($fmt, "\n")));
+    ($fmt:expr, $($arg:tt)*) => {
+        $crate::serial_print!(concat!($fmt, "\n"), $($arg)*);
     };
-    ($($arg:tt)*) => {
-        ::x86_64::instructions::interrupts::without_interrupts(|| {
-            $crate::serial_println!(interrupts_disabled $($arg)*);
-        })
-    }
 }
--- kernel/src/vga/macros.rs
+++ kernel/src/vga/macros.rs
@@ -3,41 +3,31 @@ use core::fmt;
 
 #[macro_export]
 macro_rules! print {
-    (interrupts_disabled bgcolor = $bg:expr, $($arg:tt)*) => {
+    (bgcolor = $bg:expr, $($arg:tt)*) => {
         VGA_OUT.lock().color.set_bg($bg);
         $crate::print!(interrupts_disabled $($arg)*);
     };
-    (interrupts_disabled fgcolor = $fg:expr, $($arg:tt)*) => {
+    (fgcolor = $fg:expr, $($arg:tt)*) => {
         VGA_OUT.lock().color.set_fg($fg);
         $crate::print!(interrupts_disabled $($arg)*);
     };
-    (interrupts_disabled $($arg:tt)*) => ($crate::vga::macros::_print(format_args!($($arg)*)));
-    ($($arg:tt)*) => {
-        ::x86_64::instructions::interrupts::without_interrupts(|| {
-            $crate::print!(interrupts_disabled $($arg)*);
-        })
-    }
+    ($($arg:tt)*) => ($crate::vga::macros::_print(format_args!($($arg)*)));
 }
 
 #[macro_export]
 macro_rules! println {
-    (interrupts_disabled bgcolor = $bg:expr, $($arg:tt)*) => {
+    (bgcolor = $bg:expr, $($arg:tt)*) => {
         VGA_OUT.lock().color.set_bg($bg);
-        $crate::println!(interrupts_disabled $($arg)*)
+        $crate::println!($($arg)*)
     };
-    (interrupts_disabled fgcolor = $fg:expr, $($arg:tt)*) => {
+    (fgcolor = $fg:expr, $($arg:tt)*) => {
         VGA_OUT.lock().color.set_fg($fg);
-        $crate::println!(interrupts_disabled $($arg)*);
+        $crate::println!($($arg)*);
     };
-    (interrupts_disabled $($arg:tt)*) => {
-        $crate::print!(interrupts_disabled "{}\n", format_args!($($arg)*))
-    };
-    (interrupts_disabled) => ($crate::print!(interrupts_disabled "\n"));
+    () => ($crate::print!("\n"));
     ($($arg:tt)*) => {
-        ::x86_64::instructions::interrupts::without_interrupts(|| {
-            $crate::println!(interrupts_disabled $($arg)*);
-        })
-    }
+        $crate::print!("{}\n", format_args!($($arg)*))
+    };
 }
 
 #[doc(hidden)]
